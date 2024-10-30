82b0a551fa4311d4bf2a4470e94c9a1feac4a1de
Lorenzo Bonanni <lorenzopuntobonanni@gmail.com>
2024-10-30 10:35:13 +0100

    refactoring

--- cvar/gridworld/algorithms/new_value_iteration.py
+++ cvar/gridworld/algorithms/new_value_iteration.py
@@ -7,81 +7,77 @@ from tqdm import tqdm
 from cvar.gridworld.cliffwalker import GridWorld
 
 
+def get_position_and_probabilities(action_transitions):
+    transitions_pos = []
+    transitions_probabilities = []
+    for trans in action_transitions:
+        transitions_pos.append(trans.state)
+        transitions_probabilities.append(trans.prob)
+
+    return np.array(transitions_pos), np.array(transitions_probabilities)
+
+
+def solve_problem(solver):
+    solver.print_information()
+    solution = solver.solve()
+    if solution.solve_status.value == 2:
+        print('Optimal solution found')
+        solved_xi = []
+        solved_t = []
+        for v in solver.iter_variables():
+            if v.name.startswith('xi'):
+                solved_xi.append(v.solution_value)
+            elif v.name.startswith('t'):
+                solved_t.append(v.solution_value)
+    else:
+        print('No optimal solution found')
+        exit(1)
+
+
 def value_update(world, V, id=0, alpha_set_all=None):
     V_ = copy.deepcopy(V)
     for s in tqdm(world.states(), desc='Value Update %d' % id):
         alpha_set = alpha_set_all[s.y, s.x]
         transitions = world.transitions(s)
-        solver = Model(name='cvar_value')
+        for alpha_idx, alpha in enumerate(alpha_set):
+            solver = Model(name='cvar_value')
 
-        right_ineqs = []
-        left_ineqs = []
-        xi_constraints = []
-        counter = 0
-        objective = np.zeros((len(world.ACTIONS), len(alpha_set)))
-        objectives_fn = []
-        for a in world.ACTIONS:
-            action_transitions = transitions[a]
-            transitions_pos = []
-            transitions_probabilities = []
-            for trans in action_transitions:
-                transitions_pos.append(trans.state)
-                transitions_probabilities.append(trans.prob)
+            counter = 0
+            objective = np.zeros((len(world.ACTIONS), len(alpha_set)))
 
-            transitions_pos = np.array(transitions_pos)
-            transitions_probabilities = np.array(transitions_probabilities)
-            ts = []
-            for i in range(len(alpha_set) - 1):
-                alpha_i = alpha_set[i]
-                alpha_ip1 = alpha_set[i + 1]
+            for a in world.ACTIONS:
+                transitions_pos, transitions_probabilities = get_position_and_probabilities(transitions[a])
                 n_trans = len(transitions_pos)
 
-                xi = np.array([solver.continuous_var(0, solver.infinity, f'xi_{counter+tnum}') for tnum in range(n_trans)])
-                counter += n_trans
-                t = np.array([solver.continuous_var(-1e6, 1e6, f't_{counter+tnum}') for tnum in range(n_trans)])
-                counter += n_trans
-
-                ts.append(t)
-                v_i = V_[i, transitions_pos[:, 0], transitions_pos[:, 1]]
-                v_ip1 = V_[i+1, transitions_pos[:, 0], transitions_pos[:, 1]]
-                slope = (alpha_ip1 * v_ip1 - alpha_i * v_i) / (alpha_ip1 - alpha_i)
-                right_ineq = lambda alpha_in: (alpha_i*v_i/alpha_in - slope * alpha_i/alpha_in) * transitions_probabilities
-                left_ineq = t - slope * xi * transitions_probabilities
-                right_ineqs.append(right_ineq)
-                left_ineqs.append(left_ineq)
-                xi_constraints.append(xi)
-                objectives_fn.append(sum(t))
-                solver.add_constraint(xi @ transitions_probabilities == 1)
-
-            for alpha_idx, alpha in enumerate(alpha_set):
                 if alpha == 0:
                     # when alpha is 0, the cvar is simply the worst case value, so no expectation over some distribution
                     transitions_pos = np.array(transitions_pos)
                     objective[a, alpha_idx] = min(V_[alpha_idx, transitions_pos[:, 0], transitions_pos[:, 1]])
-                else:
-                    for idx in range(len(right_ineqs)):
-                        l_ineq = left_ineqs[idx]
-                        r_ineq = right_ineqs[idx](alpha)
-                        for idx2 in range(len(l_ineq)):
-                            solver.add_constraint(l_ineq[idx2] >= r_ineq[idx2])
-                            solver.add_constraint(xi_constraints[idx][idx2] <= 1/alpha)
+                    continue
 
-        solver.maximize(sum(objectives_fn))
-        solver.print_information()
-        solution = solver.solve()
-        if solution.solve_status.value == 2:
-            print('Optimal solution found')
-            solved_xi = []
-            solved_t = []
-            # TODO understand why this is not working, xi values are 1 which violates the constraints
-            for v in solver.iter_variables():
-                if v.name.startswith('xi'):
-                    solved_xi.append(v.solution_value)
-                elif v.name.startswith('t'):
-                    solved_t.append(v.solution_value)
-        else:
-            print('No optimal solution found')
-            exit(1)
+                for i in range(len(alpha_set) - 1):
+                    alpha_i = alpha_set[i]
+                    alpha_i_next = alpha_set[i + 1]
+                    v_i = V_[i, transitions_pos[:, 0], transitions_pos[:, 1]]
+                    v_i_next = V_[i + 1, transitions_pos[:, 0], transitions_pos[:, 1]]
+                    slope = (alpha_i_next * v_i_next - alpha_i * v_i) / (alpha_i_next - alpha_i)
+
+
+                    xi = np.array([solver.continuous_var(0, solver.infinity, f'xi_{counter + tnum}') for tnum in range(n_trans)])
+                    counter += n_trans
+                    t = np.array([solver.continuous_var(-1e6, 1e6, f't_{counter + tnum}') for tnum in range(n_trans)])
+                    counter += n_trans
+
+                    right_ineq = alpha_i * v_i / alpha - slope * alpha_i / alpha * transitions_probabilities
+                    left_ineq = t - slope * xi * transitions_probabilities
+                    for idx in range(len(right_ineq)):
+                        solver.add_constraint(left_ineq[idx] >= right_ineq[idx])
+                        solver.add_constraint(xi[idx] <= 1 / alpha)
+
+                    solver.add_constraint(xi @ transitions_probabilities == 1)
+                    solver.maximize(sum(t))
+                    solve_problem(solver)
+
     return V_
 
 
